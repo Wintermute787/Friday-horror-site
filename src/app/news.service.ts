b887fd2179b9decc2df064cea631e042c9be98da
Joe Barnes <joebarnes333@gmail.com>
2019-06-21 08:42:57 -0700

    add firebase keys and services for news

---
+++ src/app/news.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {News} from './models/news.model';
+import {}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor() { }
+}
